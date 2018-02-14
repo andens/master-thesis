9285eb7453a509395edaa77cc15767e544722f23
Andreas Flöjt <andenzz@hotmail.com>
2018-02-14 11:52:39 +0100

    Debug callback prints the layer prefix before the message.

--- thesis-project/src/renderer/renderer.cpp
+++ thesis-project/src/renderer/renderer.cpp
@@ -57,7 +57,7 @@ void Renderer::create_debug_callback() {
   callback_info.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
   callback_info.pUserData = nullptr;
   callback_info.pfnCallback = [](VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objectType, uint64_t object, size_t location, int32_t messageCode, const char* pLayerPrefix, const char* pMessage, void* pUserData) -> VkBool32 {
-    std::cout << pMessage << std::endl;
+    std::cout << pLayerPrefix << " " << pMessage << std::endl;
     return VK_FALSE;
   };
 
