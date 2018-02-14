fc051c5e5b7e0f0cade4f64f169ec544d1090bf6
Andreas Flöjt <andenzz@hotmail.com>
2018-02-14 11:51:20 +0100

    Shorten instance creation by using the builder.

--- thesis-project/src/renderer/renderer.cpp
+++ thesis-project/src/renderer/renderer.cpp
@@ -6,6 +6,7 @@
 #include <vulkan-helpers/device.h>
 #include <vulkan-helpers/device_builder.h>
 #include <vulkan-helpers/instance.h>
+#include <vulkan-helpers/instance_builder.h>
 #include <vulkan-helpers/presentation_surface.h>
 #include <vulkan-helpers/vk_dispatch_tables.h>
 
@@ -34,71 +35,19 @@ Renderer::~Renderer() {
 }
 
 void Renderer::create_instance() {
-  VkApplicationInfo app_info {};
-  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  app_info.pNext = nullptr;
-  app_info.apiVersion = 0;
-  app_info.pApplicationName = nullptr;
-  app_info.applicationVersion = 0;
-  app_info.pEngineName = nullptr;
-  app_info.engineVersion = 0;
+  vk::InstanceBuilder builder;
 
-  uint32_t layer_count = 0;
-  vk_globals_->vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
-  std::vector<VkLayerProperties> available_layers(layer_count);
-  vk_globals_->vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+  builder.use_layer("VK_LAYER_LUNARG_standard_validation");
 
-  std::vector<char const*> layers {
-    "VK_LAYER_LUNARG_standard_validation"
-  };
-
-  for (auto l : layers) {
-    if (available_layers.end() == std::find_if(available_layers.begin(), available_layers.end(), [&l](VkLayerProperties const& layer) {
-      return strcmp(layer.layerName, l) == 0;
-    })) {
-      throw std::runtime_error(std::string("Vulkan instance layer `") + l + "` is not available.");
-    }
-  }
-
-  uint32_t extension_count = 0;
-  vk_globals_->vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
-  std::vector<VkExtensionProperties> available_extensions(extension_count);
-  vk_globals_->vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, available_extensions.data());
-
-  std::vector<char const*> extensions {
-    "VK_EXT_debug_report",
-    "VK_KHR_surface",
-#if defined(WIN32)
-    "VK_KHR_win32_surface"
+  builder.use_extension("VK_EXT_debug_report");
+  builder.use_extension("VK_KHR_surface");
+#if defined(_WIN32)
+  builder.use_extension("VK_KHR_win32_surface");
+#else
+#error "Unsupported OS"
 #endif
-  };
 
-  auto extension_it = extensions.begin();
-  for (auto e : extensions) {
-    if (available_extensions.end() == std::find_if(available_extensions.begin(), available_extensions.end(), [&e](VkExtensionProperties const& ext) {
-      return strcmp(ext.extensionName, e) == 0;
-    })) {
-      throw std::runtime_error(std::string("Vulkan instance extension `") + e + "` is not available.");
-    }
-  }
-
-  VkInstanceCreateInfo instance_info {};
-  instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  instance_info.pNext = nullptr;
-  instance_info.flags = 0;
-  instance_info.pApplicationInfo = &app_info;
-  instance_info.enabledLayerCount = layers.size();
-  instance_info.ppEnabledLayerNames = layers.data();
-  instance_info.enabledExtensionCount = extensions.size();
-  instance_info.ppEnabledExtensionNames = extensions.data();
-
-  VkInstance instance { VK_NULL_HANDLE };
-  VkResult result = vk_globals_->vkCreateInstance(&instance_info, nullptr, &instance);
-  if (result != VK_SUCCESS) {
-    throw std::runtime_error("Could not create Vulkan instance.");
-  }
-
-  instance_.reset(new vk::Instance(instance, vk_globals_.get()));
+  instance_ = builder.build(vk_globals_.get());
 }
 
 void Renderer::create_debug_callback() {
