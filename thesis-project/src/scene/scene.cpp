35d333286a68d33cef461a2c88926a04cfc6c20e
Andreas Flöjt <andenzz@hotmail.com>
2018-03-13 15:17:57 +0100

    Logic to alter the scene to test the incremental changes.

--- thesis-project/src/scene/scene.cpp
+++ thesis-project/src/scene/scene.cpp
@@ -4,6 +4,27 @@
 #include "../render-cache/render-cache.h"
 
 void Scene::update(float delta_time, Renderer& renderer) {
+  // Simple logic to stop and start rendering the first object in intervals
+  // of one second. Every new reappearance switches mesh to demonstrate that
+  // draw calls can not only be individually updated, they can also completely
+  // alter what is rendered.
+  static float time = 0.0f;
+  static bool stop_rendering = false;
+  static bool box = true;
+  time += delta_time;
+  if (time > 1.0f) {
+    time = 0.0f;
+    stop_rendering = !stop_rendering;
+    renderer.borrow_render_cache([](RenderCache& cache) {
+      if (stop_rendering) {
+        cache.stop_rendering(0);
+      }
+      else {
+        box = !box;
+        cache.start_rendering(0, box ? RenderObject::Box : RenderObject::Sphere);
+      }
+    });
+  }
 }
 
 Scene::Scene(Renderer& renderer) {
