b54afc79b176a3c7cd3ea37cbce9fcb550ea8719
Andreas Flöjt <andenzz@hotmail.com>
2018-03-29 16:47:58 +0200

    Show frame time history in the GUI to see how it evolves.

--- thesis-project/src/scene/scene.cpp
+++ thesis-project/src/scene/scene.cpp
@@ -1,9 +1,29 @@
 #include "scene.h"
 
+#include <algorithm>
+#include <imgui.h>
 #include "../renderer/renderer.h"
 #include "../render-cache/render-cache.h"
 
 void Scene::update(float delta_time, Renderer& renderer) {
+  // Get the previous frame render time to update graph
+  accumulated_render_time_ += renderer.measured_time();
+  accumulated_frames_++;
+  accumulation_timer_ += delta_time;
+  if (accumulation_timer_ > time_per_accumulation_) {
+    accumulation_timer_ -= time_per_accumulation_;
+    render_time_history_.pop_front();
+    render_time_history_.push_back(static_cast<float>(accumulated_render_time_ / accumulated_frames_));
+    accumulated_render_time_ = 0.0;
+    accumulated_frames_ = 0;
+    largest_history_entry_ = 0.0f;
+    std::for_each(render_time_history_.begin(), render_time_history_.end(), [this](float val) {
+      if (val > largest_history_entry_) {
+        largest_history_entry_ = val;
+      }
+    });
+  }
+
   // Simple logic to stop and start rendering the first object in intervals
   // of one second. Every new reappearance switches mesh to demonstrate that
   // draw calls can not only be individually updated, they can also completely
@@ -25,6 +45,21 @@ void Scene::update(float delta_time, Renderer& renderer) {
       }
     });
   }
+
+  if (ImGui::Begin("Controls")) {
+    if (ImGui::CollapsingHeader("Render time", ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_DefaultOpen)) {
+      ImVec2 plot_extent { ImGui::GetContentRegionAvailWidth() - 70, 100 };
+      ImGui::PlotLines("", [](void* data, int idx) -> float {
+        auto frame_times = reinterpret_cast<std::deque<float>*>(data);
+        return (*frame_times)[idx];
+      }, &render_time_history_, render_time_history_.size(), 0, nullptr, 0.0f, FLT_MAX, plot_extent);
+
+      ImGui::SameLine();
+
+      ImGui::Text("%-3.4f ms", largest_history_entry_);
+    }
+  }
+  ImGui::End();
 }
 
 Scene::Scene(Renderer& renderer) {
@@ -35,4 +70,9 @@ Scene::Scene(Renderer& renderer) {
     cache.start_rendering(0, RenderObject::Box, RenderCache::Pipeline::Alpha);
     cache.start_rendering(1, RenderObject::Sphere, RenderCache::Pipeline::Alpha);
   });
+
+  uint32_t entries = history_time_span_ / time_per_accumulation_;
+  for (uint32_t i = 0; i < entries; ++i) {
+    render_time_history_.push_back(0.0f);
+  }
 }
--- thesis-project/src/scene/scene.h
+++ thesis-project/src/scene/scene.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <deque>
+
 class Renderer;
 
 class Scene {
@@ -7,4 +9,14 @@ public:
   void update(float delta_time, Renderer& renderer);
 
   Scene(Renderer& renderer);
+
+private:
+  double accumulated_render_time_ { 0.0 };
+  uint32_t accumulated_frames_ { 0 };
+  float accumulation_timer_ { 0.0f };
+  const float time_per_accumulation_ { 0.25f };
+
+  std::deque<float> render_time_history_;
+  const float history_time_span_ { 60.0f }; // Seconds of history shown
+  float largest_history_entry_ { 0.0f };
 };
