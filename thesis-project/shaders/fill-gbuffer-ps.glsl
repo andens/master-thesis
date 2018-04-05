2a7c85245b58f73700bfbe80b4659aaf5316e8a1
Andreas Flöjt <andenzz@hotmail.com>
2018-04-05 16:08:07 +0200

    Write to the normal buffer from the pixel shader.

--- thesis-project/shaders/fill-gbuffer-ps.glsl
+++ thesis-project/shaders/fill-gbuffer-ps.glsl
@@ -4,7 +4,9 @@ layout(location = 0) in vec2 i_TexC;
 //layout(location = 1) in vec3 i_NormV;
 
 layout(location = 0) out vec4 o_Albedo;
+layout(location = 1) out vec4 o_Normal;
 
 void main() {
   o_Albedo = vec4(i_TexC, 0.0f, 1.0f);
+  o_Normal = vec4(0.0f);
 }
