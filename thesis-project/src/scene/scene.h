73d1d870334d3003299d16d2fc03bef6bb5e7d86
Andreas Flöjt <andenzz@hotmail.com>
2018-04-12 14:13:37 +0200

    Reduce monitor history span length and increase sample frequency.

--- thesis-project/src/scene/scene.h
+++ thesis-project/src/scene/scene.h
@@ -19,10 +19,10 @@ private:
   double accumulated_render_time_ { 0.0 };
   uint32_t accumulated_frames_ { 0 };
   float accumulation_timer_ { 0.0f };
-  const float time_per_accumulation_ { 0.25f };
+  const float time_per_accumulation_ { 0.1f };
 
   std::deque<float> render_time_history_;
-  const float history_time_span_ { 60.0f }; // Seconds of history shown
+  const float history_time_span_ { 30.0f }; // Seconds of history shown
   float largest_history_entry_ { 0.0f };
 
   int pipeline_switches_ { 1 };
